8b3553e7b2cec3c0d00dcb37292798c3627e2a92
Mihai Dogaru <dogarumihai2537@gmail.com>
2026-01-12 15:51:40 +0200

    Hide cart when clicking outside

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ const btnMenuCloseMobileEl = document.getElementById("btnMenuCloseMobile");
 const mainEl = document.querySelector("main");
 
 const btnCart = document.querySelector(".cart");
+const cartContentEl = document.querySelector(".cart-content");
 const headerEl = document.querySelector("header");
-
-console.log(headerEl);
 
 btnMenuMobileEl.addEventListener("click", (ev) => {
   mainEl.classList.toggle("open-menu");
@@ -18,5 +17,15 @@ btnMenuCloseMobileEl.addEventListener("click", (ev) => {
 });
 
 btnCart.addEventListener("click", (ev) => {
+  console.log("Clicked");
+  ev.stopPropagation();
   headerEl.classList.toggle("cart-visible");
+});
+
+cartContentEl.addEventListener("click", (ev) => {
+  ev.stopPropagation();
+});
+
+document.addEventListener("click", () => {
+  headerEl.classList.remove("cart-visible");
 });
